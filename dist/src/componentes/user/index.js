29bc765e789da03f7e802e4e26632bcd2e18f8a0
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2023-01-18 20:47:58 -0500

    ADD: Estructure components

---
+++ dist/src/componentes/user/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const controller_1 = require("./controller");
+const userRouter = (0, express_1.Router)();
+userRouter.get("/", controller_1.findAll);
+userRouter.post("/", controller_1.store);
+exports.default = userRouter;
+//# sourceMappingURL=index.js.map
