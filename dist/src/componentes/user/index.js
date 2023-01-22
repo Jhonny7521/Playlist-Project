062441bf635a036487e394a170c528794ba7e173
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2023-01-21 20:35:50 -0500

    ADD: password encryption and token verification :fire:

--- dist/src/componentes/user/index.js
+++ dist/src/componentes/user/index.js
@@ -4,6 +4,7 @@ const express_1 = require("express");
 const controller_1 = require("./controller");
 const userRouter = (0, express_1.Router)();
 userRouter.get("/", controller_1.findAll);
-userRouter.post("/", controller_1.store);
+userRouter.post("/signup", controller_1.signup);
+userRouter.post("/login", controller_1.login);
 exports.default = userRouter;
 //# sourceMappingURL=index.js.map
